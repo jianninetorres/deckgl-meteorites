5930acea0d0c362237783cdef08fe25a7a91daaf
Jiannine Torres <jianninetorres@gmail.com>
2022-07-02 15:16:12 -0400

    Update Tooltip styles

--- src/components/tooltip/Tooltip.jsx
+++ src/components/tooltip/Tooltip.jsx
@@ -8,25 +8,65 @@ const Tooltip = (props) => {
           style={{
             position: "absolute",
             zIndex: 10000,
-            background: "#ffffff",
+            background: "rgba(255, 255, 255, 0.75)",
             pointerEvents: "none",
-            borderRadius: "5px",
+            borderRadius: "10px",
             left: props.hover.x,
             top: props.hover.y,
             width: 250,
+            lineHeight: 1.5,
           }}
         >
           <ul className="hovered-object-data">
             <li>
               <h4>{props.hover.object.name}</h4>
             </li>
-            <li>id: {props.hover.object.id}</li>
-            <li>Observation: {props.hover.object.fall}</li>
-            <li>Year: {props.hover.object.year}</li>
-            <li>Mass: {props.hover.object.mass} grams</li>
+            <li>
+              <b>id</b>: {props.hover.object.id}
+            </li>
+            <li>
+              <b>Observation</b>: {props.hover.object.fall}
+            </li>
+            <li>
+              <b>Year</b>: {props.hover.object.year}
+            </li>
+            <li>
+              <b>Mass</b>: {props.hover.object.mass} grams
+            </li>
           </ul>
         </div>
       )}
+      {/* <div
+        style={{
+          position: "absolute",
+          zIndex: 10000,
+          background: "rgba(255, 255, 255, 0.75)",
+          pointerEvents: "none",
+          borderRadius: "10px",
+          left: props.hover.x,
+          top: props.hover.y,
+          width: 250,
+          lineHeight: 1.5,
+        }}
+      >
+        <ul className="hovered-object-data">
+          <li>
+            <h4>Toronto</h4>
+          </li>
+          <li>
+            <b>id</b>: 123
+          </li>
+          <li>
+            <b>Observation</b>: Fell
+          </li>
+          <li>
+            <b>Year</b>: 1990
+          </li>
+          <li>
+            <b>Mass</b>: 245 grams
+          </li>
+        </ul>
+      </div> */}
     </div>
   );
 };
